0cb5e97df5670ba0700270a163b48fbcaea4c595
GUSTAVO DE OLIVEIRA ALVES <gustavo.oalves@senacsp.edu.br>
2018-04-11 09:23:40 -0300

    Realizando a soma

--- Program.cs
+++ Program.cs
@@ -9,12 +9,14 @@ namespace PrimeiroGit
             //Declarção das variáveis para guardar os valores
             //que o usuário entrará
             double valor1,valor2,resultado;
-            
+
             Console.WriteLine("Digite um valor: ");
             valor1 = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Entre com outro valor: ");
             valor2 = double.Parse(Console.ReadLine());
+
+            resultado = valor1+valor2;
         }
     }
 }
